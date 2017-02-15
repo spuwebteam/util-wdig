2ba0f2e212b03fbf7df61924b7e556d90bc5b1b4
Logan Kyte <logan.kyte@seattle.gov>
2017-02-14 16:13:06 -0800

    Folder cleanup and nav accessibility edits

--- js/script.js
+++ js/script.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
-    var recurseNodes = function(node, html) {
+    var recurseNodes = function(node, html, ariaLabeledBy) {
         var subs = node.m_subNodes,
             len = subs.length;
-        html+= '<ul>';
+        html+= '<ul';
+        if (ariaLabeledBy) {
+            html+= ' aria-labelledby="' + ariaLabeledBy + '"';  
+        }
+        html+= '>';
         for (var i = 0; i < len; i++) {
-        	var hasChildren = subs[i].m_subNodes.length;
-            html += '<li';
+        	var hasChildren = subs[i].m_subNodes.length,
+                idLabel = 'node-' + subs[i].m_id;
+            html += '<li id="' + idLabel + '"';  
             if (hasChildren) {
             	html += ' class="hasChildren"';
             }
-            html += '><a target="_blank" href="' + subs[i].m_href + '">' + subs[i].m_label;
+            html += '><a target="_blank"';
+            if (hasChildren) {
+                html += ' aria-haspopup="true" aria-expanded="false"';
+            }
+            html += ' href="' + subs[i].m_href + '">' + subs[i].m_label;
             if (hasChildren) {
             	html += '<i class="fa fa-chevron-circle-down"></i>';
             }
             html += '</a>';
             if (hasChildren) {
-            	html += recurseNodes(subs[i], '');
+            	html += recurseNodes(subs[i], '', idLabel);
             }
             html += '</li>';
         }
@@ -25,7 +34,7 @@ $(document).ready(function(){
     
     var $lut = $('#nav'),
         $lutLi = $('#nav li'),
-        $lutHTML = '<ul id="start"><li class="top hasChildren"><a href id="label">Select your category<i class="fa fa-chevron-circle-down"></i></a>';
+        $lutHTML = '<ul id="start"><li class="top hasChildren"><a href id="label" aria-expanded="true">Select your category<i class="fa fa-chevron-circle-down"></i></a>';
         $lutHTML += recurseNodes(g_navNode_0_8, '');
         $lutHTML += '</li></ul>';
         
@@ -34,7 +43,6 @@ $(document).ready(function(){
 
 	$('#nav ul li a').on('click', function(e){
 		$('#label').html('Start over');
-	    
 	    if ($(this).parent().hasClass('top') || $(this).parent().hasClass('hasChildren')) {
 	    	e.preventDefault(); // prevents default to anchor tag
 	    } else {
@@ -42,10 +50,13 @@ $(document).ready(function(){
 	    }
 
     	if ($(this).siblings('ul').hasClass('active')) { //clicking on previous parent to get its children
+            $(this).siblings('ul').children('li').children('a').attr('aria-expanded', 'false'); // adds false to previously expanded a for sr
 			$(this).siblings('ul').children('li').find('ul').removeClass('active'); // find all ul below and remove class active
             $(this).siblings('ul').find('li').removeClass('selected'); // removes selected color from all parent li 
 			$(this).siblings('ul').children('li').slideDown();
 		} else { // clicking on li to bring its children
+            $(this).attr('aria-expanded', 'true');     // adds true to newly expanded a for SR accesibility
+            $(this).parents('li').parents('ul').parents('li').children('a').attr('aria-expanded', 'false'); // adds false to previously expanded a for sr
 			$(this).siblings('ul').children('li').hide(); // hide li's that are about to slide down
 			$(this).siblings('ul').addClass('active');
             $(this).closest('li').addClass('selected'); //adds selected color to clicked li
